2179b5f1670ecd249cc682ecb1bcea64edfa9465
kstoneb1 <kstoneb1@gmail.com>
2020-07-07 22:24:05 -0700

    Week6 section B/C

--- DSC520/wk06/week_06_StoneburnerKurt_Housing.R
+++ DSC520/wk06/week_06_StoneburnerKurt_Housing.R
@@ -74,37 +74,83 @@ housing_df <- removeColsFromDF(raw_housing_df,c("lon",
                                                 "bath_half_count",
                                                 "bath_3qtr_count",
                                                 "present_use"))[,-1]
+
+housing_df <- removeColsFromDF(raw_housing_df,c("lon",
+                                                "lat",
+                                                "addr_full",
+                                                "ctyname",
+                                                "postalctyn",
+                                                "prop_type",
+                                                "year_renovated",
+                                                "current_zoning",
+                                                "bath_full_count",
+                                                "bath_half_count",
+                                                "bath_3qtr_count",
+                                                "sitetype",
+                                                "sale_warning",
+                                                "present_use",
+                                                "sale_reason",
+                                                "sale_instrument",
+                                                "Sale.Date"))[,-1]
+
 #### Combine the bathrooms
 bath_total <- raw_housing_df$bath_full_count + (raw_housing_df$bath_half_count *.5) + (raw_housing_df$bath_3qtr_count *.75)
 
 housing_df <- cbind(housing_df,bath_total)
+tail(housing_df)
 
-whole_year_built <- vapply(housing_df$year_built, function(x){
-  thisDate <- paste0("1/1/",x)
-  
-  return ( thisDate  )
-  }, character(1) )
+cor(housing_df)[1,]
 
-whole_year_built <- as.Date( whole_year_built, tryFormats=c("%m/%d/%Y") )
-whole_year_built <- format(as.Date(whole_year_built, format="%Y/%m/%d"),"%Y")
+### Calculate the age of the house (Sale Date - sale_year)
+cor(housing_df)[1,]
 
+
+### b.
+### base_lm: Predicts Sale Price based on lot lot size
+salePrice_base_lm <-  lm(Sale.Price ~ sq_ft_lot, data=housing_df)
+summary(salePrice_base_lm)
+salePrice_naieve_lm <-  lm(Sale.Price ~ zip5 + bedrooms + bath_total + square_feet_total_living, data=housing_df)
+summary(salePrice_naieve_lm)
+salePrice_complex_4_lm <-    lm(Sale.Price ~ square_feet_total_living + building_grade + bath_total, data=housing_df)
+summary(salePrice_complex_4_lm)
+salePrice_complex_6_lm <-    lm(Sale.Price ~ square_feet_total_living + building_grade + bedrooms + bath_total + year_built, data=housing_df)
+summary(salePrice_complex_6_lm)
+
+hist(housing_df$year_built)
+hist(housing_df$house_age)
+cor(housing_df$year_built,housing_df$house_age)
+
+## c. Execute a summary() function on two variables defined in the previous step to compare the model results. What are the R2 and Adjusted R2 statistics? Explain what these results tell you about the overall model. Did the inclusion of the additional predictors help explain any large variations found in Sale Price?
+summary(salePrice_base_lm)
+cor(housing_df[2:length(housing_df)])
+summary(salePrice_complex_lm)
+head(housing_df)
+
+cor(num_housing_df)[1,]
+head(num_housing_df)
+
+unique(bath_total)
+
+hist(bath_total)
+plot(bath_total,housing_df$Sale.Price)
+plot(raw_housing_df$bath_full_count,housing_df$Sale.Price)
+
+summary(raw_housing_df$bath_full_count)
+summary(bath_total)
+
+iqr_dumper_df <- housing_df[which(housing_df$bath_total <= 5),]
+iqr_dumper_df <- iqr_dumper_df[which(iqr_dumper_df$bath_total > 1),]
+nrow(iqr_dumper_df)
+cor(iqr_dumper_df)[1,]
+plot(iqr_dumper_df$bath_total,iqr_dumper_df$Sale.Price)
 housing_df$Sale.Date <-  as.Date(housing_df$Sale.Date,  tryFormats = c("%m/%d/%Y") )
-housing_df$year_built <- whole_year_built
-
-year_built <- as.numeric(housing_df$year_built)
-(housing_df$year_built - sale_year)
-
-sale_year <- as.numeric(sale_year)
-
-sale_year - whole_year_built
-
-sale_year <- format(as.Date(housing_df$Sale.Date, format="%Y/%m/%d"),"%Y")
-
+sale_year
 housing_df <- cbind(housing_df,sale_year)
 housing_df <- cbind(housing_df,year_built)
 
-housing_df
-sale_year - housing_df$year_built
+
+
+housing_df$year_built
 
 housing_df[ which(housing_df$sale_year < housing_df$year_built),]
 
@@ -155,9 +201,28 @@ coded_zoning <- vapply(raw_housing_df$current_zoning, function(x){
   thisIndex <- match(x,zoning_zones)
   return(thisIndex)},numeric(1))
 
-coded_zoning
-
+cor(cbind(housing_df,coded_zoning))[1,]
+tail(housing_df)
 hist(coded_zoning)
 hist(raw_housing_df$present_use)
 
 cor(coded_zoning,raw_housing_df$present_use)
+
+year_built <- as.numeric(raw_housing_df$year_built)
+year_built
+raw_housing_df$Sale.Date
+sale_year <- format(as.Date(raw_housing_df$Sale.Date, format="%m/%d/%Y"),"%Y")
+sale_year
+sale_year <- as.numeric(sale_year)
+sale_year
+house_age <- (sale_year - year_built)
+
+house_age
+
+house_age <- vapply(house_age, function(x){
+  if (x < 0 ) { return(0)}
+  return(x)
+},numeric(1))
+
+cor(housing_df)[1,]
+housing_df <- cbind(housing_df,house_age)
